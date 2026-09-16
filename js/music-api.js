51ada56321f852fd5832fcc29690c90b7a4f75ed
JannickDEV <211101254+JannickDEV@users.noreply.github.com>
2026-09-16 12:02:44 +0200

    Revert "debug: instrument the click-to-play chain to find the actual stopping point"
    This reverts commit 40f55829ed1760007870e91f496e7c3654a88382.

--- js/music-api.js
+++ js/music-api.js
@@ -369,7 +369,6 @@ export class MusicAPI {
 
     // Stream methods
     async getStreamUrl(id, quality, options = {}) {
-        console.error('[DEBUG] MusicAPI.getStreamUrl entered', { id, quality });
         if (String(id).startsWith('sc_')) {
             const { soundCloudAPI } = await import('./soundcloud-api.js');
             return await soundCloudAPI.getStreamUrl(id);
@@ -377,17 +376,9 @@ export class MusicAPI {
         const api = this.getAPI();
         let appleTrack = options?.track || this.getCachedAppleTrack(id);
         if (!appleTrack && (this.isAppleId(id, 'track') || this.isAppleId(id, 'video') || this.isAppleId(id))) {
-            console.error('[DEBUG] resolving apple metadata for', id);
-            appleTrack = await this.getTrackMetadata(id).catch((e) => {
-                console.error('[DEBUG] apple getTrackMetadata failed', e);
-                return null;
-            });
-            console.error('[DEBUG] apple metadata resolved:', !!appleTrack, appleTrack?.isrc);
+            appleTrack = await this.getTrackMetadata(id).catch(() => null);
         }
-        if (appleTrack) {
-            console.error('[DEBUG] calling api.getStreamUrl with apple track', appleTrack.id);
-            return api.getStreamUrl(id, quality, { ...options, track: appleTrack });
-        }
+        if (appleTrack) return api.getStreamUrl(id, quality, { ...options, track: appleTrack });
         const cleanId = this.stripProviderPrefix(id);
         return api.getStreamUrl(cleanId, quality, options);
     }
